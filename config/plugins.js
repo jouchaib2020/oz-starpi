d1b7734903e43e4e14838a9bc1115fb49b1f8027
JAOUAD OUCHAIB <76879201+jouchaib2020@users.noreply.github.com>
2024-05-06 12:18:56 +0200

    added site map plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = () => ({
+    'sitemap': {
+        enabled: true,
+        config: {
+          cron: '0 0 0 * * *',
+          limit: 45000,
+          xsl: true,
+          autoGenerate: false,
+          caching: true,
+          allowedFields: ['id', 'uid'],
+          excludedTypes: [],
+        },
+      },
+});
